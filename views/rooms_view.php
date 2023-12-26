672ad1cca830dcb5a5faafb94e8c76b1220b3c07
HAFSA159 <hafsaelmoatassim99@gmail.com>
2023-12-26 16:16:17 +0100

    removed the useless fields

--- views/rooms_view.php
+++ views/rooms_view.php
@@ -21,36 +21,11 @@
                         ></path>
                     </svg>
                 </div>
-                <div class="ml-2 font-bold text-2xl">QuickChat</div>
-            </div>
-            <div
-                    class="flex flex-col items-center bg-indigo-100 border border-gray-200 mt-4 w-full py-6 px-4 rounded-lg"
-            >
-                <div class="h-20 w-20 rounded-full border overflow-hidden">
-                    <img
-                            src="https://avatars3.githubusercontent.com/u/2763884?s=128"
-                            alt="Avatar"
-                            class="h-full w-full"
-                    />
-                </div>
-                <div class="text-sm font-semibold mt-2">Aminos Co.</div>
-                <div class="text-xs text-gray-500">Lead UI/UX Designer</div>
-                <div class="flex flex-row items-center mt-3">
-                    <div
-                            class="flex flex-col justify-center h-4 w-8 bg-indigo-500 rounded-full"
-                    >
-                        <div class="h-3 w-3 bg-white rounded-full self-end mr-1"></div>
-                    </div>
-                    <div class="leading-none ml-1 text-xs">Active</div>
-                </div>
+                <div class="ml-2 font-bold text-2xl">CatChat</div>
             </div>
             <div class="flex flex-col mt-8">
                 <div class="flex flex-row items-center justify-between text-xs">
                     <span class="font-bold">Active Conversations</span>
-                    <span
-                            class="flex items-center justify-center bg-gray-300 h-4 w-4 rounded-full"
-                    >4</span
-                    >
                 </div>
                 <div class="flex flex-col space-y-1 mt-4 -mx-2 h-48 overflow-y-auto">
                     <button
@@ -110,11 +85,7 @@
                     </button>
                 </div>
                 <div class="flex flex-row items-center justify-between text-xs mt-6">
-                    <span class="font-bold">Archivied</span>
-                    <span
-                            class="flex items-center justify-center bg-gray-300 h-4 w-4 rounded-full"
-                    >7</span
-                    >
+                    <span class="font-bold">Rooms</span>
                 </div>
                 <div class="flex flex-col space-y-1 mt-4 -mx-2">
                     <button
