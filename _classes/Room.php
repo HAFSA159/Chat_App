96cb799582475058a8513cff5d1e8d4cb3056d41
HAFSA159 <hafsaelmoatassim99@gmail.com>
2023-12-28 12:01:04 +0100

    show the users

--- _classes/Room.php
+++ _classes/Room.php
@@ -18,4 +18,25 @@
                 return $rus->fetch_all(MYSQLI_ASSOC);
 
             }
+                static public function GetAllUsers() {
+                    global $db;
+                    $id =   $query = "SELECT * from users ";
+                    $rus=$db->query($query);
+                    return $rus->fetch_all(MYSQLI_ASSOC);
+
+                }
+//            static function GetAllUsers(){
+//                global $db;
+//                $sql = "SELECT * FROM users";
+//                $stmt = $db->prepare($sql);
+//
+//                $stmt->execute();
+//
+//                $result = $stmt->get_result();
+//
+//                $userData = $result->fetch_all(MYSQLI_ASSOC);
+//
+//                $stmt->close();
+//                return $userData;
+//            }
     }
